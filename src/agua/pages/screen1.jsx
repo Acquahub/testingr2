71caf9b4118d355b307c3d1f37e3a43b1af5ed90
david-dom <113909984+david-dom@users.noreply.github.com>
2023-08-21 16:34:57 -0500

    My commit message

--- src/agua/pages/screen1.jsx
+++ src/agua/pages/screen1.jsx
@@ -10,7 +10,6 @@ function Screen1() {
     return(
         <div className={styles["Screen1"]}>
             
-<div id="text-1-2539e06733c24481adf87d50fc5d83bb" className={styles["text-1-2539e06733c24481adf87d50fc5d83bb"]} >[Text]</div>
         </div>
     );
 }
